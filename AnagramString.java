1cf7b88ab26a05f2b4afbf9d2d826f5954cfe691
mounika7095738848 <125808266+mounika7095738848@users.noreply.github.com>
2023-03-17 15:52:04 +0530

    Add files via upload

---
+++ AnagramString.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramString {
+	
+	public static void main(String[] args) {
+		Scanner input =new Scanner(System.in);
+		
+		System.out.print("Enter First String: ");
+		String str1 = input.nextLine();
+		System.out.print("Enter Second String: ");
+		String str2 = input.nextLine();
+		
+		if(str1.length() == str2.length())   
+		{
+		char[]charArray1 =str1.toCharArray();
+		char[]charArray2 =str2.toCharArray();
+		
+		
+		Arrays.sort(charArray1);
+		Arrays.sort(charArray2);
+		
+		boolean result =Arrays.equals(charArray1, charArray2);
+		
+		if(result) {
+			System.out.println(str1 + " and " + str2 + " are anagram.");
+		}
+		else {
+			System.out.println(str1 + " and " + str2 + " are not anagram.");
+		}
+		
+		}
+		input.close();
+		
+		
+	}
+
+}
